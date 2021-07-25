bfda26ebfe0925c66484ccc9e4a0278627e79aee
SirShine <50844675+SirShine@users.noreply.github.com>
2021-07-25 21:35:04 +0200

    Updated to new Specification

--- Util/Decoding.js
+++ Util/Decoding.js
@@ -1,15 +1,9 @@
-// ⚸ - \u26B8 - Seperation Character
-// ᛔ - \u16D4 - 200
-// ᛤ - \u16E4 - 50
-// ᚸ - \u16B8 - 10
-// ᚻ - \u16BB - 5
-// ᚤ - \u16A4 - 1
-// ᛗ - \u16D7 - 0
-
 module.exports.edgeDecode = function(str) {
+    
     var charArr = str.split("\u26B8");
     var returnArr = [];
-    charArr.pop();
+
+    charArr.filter((e) => e.length > 0);
 
     charArr.forEach((e) => {
 
@@ -26,7 +20,7 @@ module.exports.edgeDecode = function(str) {
 
         });
 
-        returnArr.push(String.fromCharCode(charCode));
+        returnArr.push(String.fromCharCode(parseInt(charCode, 8).toString(10)));
 
     });
 
